e91a93fb9e77a24497ebfc052e87adadaa9fa350
Pasquale Musella <pasquale.musella@cern.ch>
2017-09-15 18:10:44 +0200

    added zmm_8TeV dataset

--- GAN/cms_datasets.py
+++ GAN/cms_datasets.py
@@ -4,7 +4,9 @@ import os
 
 from sklearn.model_selection import train_test_split
 
-zee_data_repo="root://t3dcachedb03.psi.ch//pnfs/psi.ch/cms/trivcat/store/user/musella/zee_data_mc"
+zee_data_repo="root://t3se01.psi.ch:1094//store/user/musella/zee_data_mc"
+
+zmm_8TeV_data_repo="root://t3se01.psi.ch:1094//store/user/musella/zmm_8TeV"
 
 def read_hdf(fname,data_repo=None):
     if not os.path.exists(fname) and data_repo != None:
@@ -19,6 +21,20 @@ def load_zee(version,selection=None):
         version_data,version_mc = version,version
     data = read_hdf('data_%s.hd5' % version_data,data_repo=zee_data_repo)
     mc   = read_hdf('mc_%s.hd5' % version_mc,data_repo=zee_data_repo)
+    
+    if selection:
+        data = data.query(selection)
+        mc   = mc.query(selection)
+
+    return data,mc
+
+def load_zmm_8TeV(version,selection=None):
+    if type(version) == tuple or type(version) == list:
+        version_data,version_mc = version
+    else:
+        version_data,version_mc = version,version
+    data = read_hdf('data_%s.hd5' % version_data,data_repo=zmm_8TeV_data_repo)
+    mc   = read_hdf('mc_%s.hd5' % version_mc,data_repo=zmm_8TeV_data_repo)
     
     if selection:
         data = data.query(selection)
